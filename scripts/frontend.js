0458547a61114dfabdc6d0846ec5423648efd8e4
Rooki <34943569+Pdzly@users.noreply.github.com>
2021-12-07 16:06:12 +0100

    Den Karusel schöner gemacht

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -1,9 +1,32 @@
-
-function loaddata(gld){
+var glider = null
+function loaddata(gld) {
     console.log("inside")
-    var glid = new Glide(".glide", {
+    glider = new Glide(".glide", {
+        breakpoints: {
+            1.536: {
+                perView: 3
+            },
+            1024: {
+                perView: 2
+            },
+            512: {
+                perView: 1
+            }
+        },
         type: 'carousel',
         startAt: 0,
-        perView: 3
+        perView: 3,
+        peek: 50,
+        animationDuration: 800,
+        animationTimingFunc: "ease",
+        hoverpause: false,
+        perTouch: 3,
+        focusAt: 'center',
     }).mount()
+}
+
+function domove(move){
+    if(glider != null){
+        glider.go(move)
+    }
 }
