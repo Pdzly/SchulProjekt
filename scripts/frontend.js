968c9a17b3201f6ae4b44b5e32e3406acc5e95e1
Rooki <34943569+Pdzly@users.noreply.github.com>
2022-01-07 11:17:31 +0100

    fixed scss

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -1,32 +1,33 @@
 var glider = null
-function loaddata(gld) {
-    console.log("inside")
-    glider = new Glide(".glide", {
-        breakpoints: {
-            1700: {
-                perView: 2,
-                peek: 150,
-            },
-            1000: {
-                perView: 1,
-                peek: 100,
-            },
-            500: {
-                perView: 1,
-                peek: 0,
 
-            }
+var glideropts = {
+    breakpoints: {
+        1700: {
+            perView: 2,
+            peek: 150,
         },
-        type: 'carousel',
-        startAt: 0,
-        perView: 4,
-        peek: 250,
-        animationDuration: 800,
-        animationTimingFunc: "ease",
-        hoverpause: false,
-        perTouch: 3,
-        focusAt: 'center',
-    }).mount()
+        1000: {
+            perView: 1,
+            peek: 100,
+        },
+        500: {
+            perView: 1,
+            peek: 0,
+
+        }
+    },
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    peek: 250,
+    animationDuration: 800,
+    animationTimingFunc: "ease",
+    hoverpause: false,
+    perTouch: 3,
+    focusAt: 'center',
+}
+function loaddata(gld) {
+    glider = new Glide(".glide", glideropts).mount()
 }
 
 function domove(move){
@@ -37,4 +38,8 @@ function domove(move){
 
 function opendetails(){
     
+}
+
+document.onresize = function(ev){
+    glider = new Glide(".glide", glideropts).mount()
 }
